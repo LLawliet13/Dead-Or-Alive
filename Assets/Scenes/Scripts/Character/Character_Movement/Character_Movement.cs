096ac2f5a204d96375c7199acc5825224ee4334c
tuanldhe150858 <tuanldhe150858@fpt.edu.vn>
2023-01-28 00:01:17 +0700

    (tuan) flip char, create map prefab
    1. flip char when turn left or right
    2. push map into prefab to avoid redundancy when loop

--- Assets/Scenes/Scripts/Character/Character_Movement/Character_Movement.cs
+++ Assets/Scenes/Scripts/Character/Character_Movement/Character_Movement.cs
@@ -6,13 +6,20 @@ public class Character_Movement : MonoBehaviour
 {
     public int speed;
     public int rotationSpeed;
-    public GameObject _charController;
+    public GameObject weaponParent;
+    public GameObject playerSprite;
     private ManageJoystick _mngrJoystick;
     private float inputX, inputY;
+    private bool facingRight = true;
     // Start is called before the first frame update
     void Start()
     {
         _mngrJoystick = GameObject.Find("imgJoystickBg").GetComponent<ManageJoystick>();
+        if(playerSprite == null || weaponParent == null)
+        {
+            playerSprite = GameObject.FindGameObjectWithTag("playerSprite");
+            weaponParent = GameObject.FindGameObjectWithTag("weaponParent");
+        }
     }
 
     // Update is called once per frame
@@ -23,6 +30,16 @@ public class Character_Movement : MonoBehaviour
         //char move
         if(inputX != 0 && inputY != 0)
         {
+            if (inputX > 0 && !facingRight)
+            {
+                Flip();
+                Debug.Log("right");
+            }
+            else if (inputX < 0 && facingRight)
+            {
+                Flip();
+                Debug.Log("left");
+            }
             Vector2 movementDirection = new Vector2(inputX, inputY);
             float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
             movementDirection.Normalize();
@@ -30,8 +47,18 @@ public class Character_Movement : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
             //----------
             Quaternion fixRotation = Quaternion.Euler(0, 0, toRotation.eulerAngles.z + 90);
-            _charController.transform.rotation = Quaternion.RotateTowards(_charController.transform.rotation, fixRotation, rotationSpeed * Time.deltaTime);
+            weaponParent.transform.rotation = Quaternion.RotateTowards(weaponParent.transform.rotation, fixRotation, rotationSpeed * Time.deltaTime);
         }
-        
+    }
+    private void Flip()
+    {
+        Debug.Log("find");
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = playerSprite.transform.localScale;
+        theScale.x *= -1;
+        playerSprite.transform.localScale = theScale;
     }
 }
