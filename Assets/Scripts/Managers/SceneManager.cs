65bbf27410cb7a36dc2007ba9a9e747e6c0d56ec
tuanldhe150858 <tuanldhe150858@fpt.edu.vn>
2023-03-13 13:29:43 +0700

    update ui for hp and exp

--- Assets/Scripts/Managers/SceneManager.cs
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -12,6 +12,17 @@ public class SceneManager : MonoBehaviour
     private int TotalExpToNextLevel;
     private int Point;
     private int PlayerLevel;
+
+    private List<IPlayerObserver> observers = new List<IPlayerObserver>();
+    public void AddObserver(IPlayerObserver observer)
+    {
+        observers.Add(observer);
+    }
+    public void RemoveObserver(IPlayerObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
     public void SetPlayerLevel(int playerLevel)
     {
         PlayerLevel = playerLevel;
@@ -30,6 +41,7 @@ public class SceneManager : MonoBehaviour
         PlayerLevel += 1;
         GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterStatus>().LevelEffect();
         getTotalExpToLevelUp();
+        Debug.Log("PlayerLevel: " + PlayerLevel + "      TotalExp: " + TotalExpToNextLevel);
     }
 
     private float SimulateTime;
@@ -43,6 +55,7 @@ public class SceneManager : MonoBehaviour
         bossSpawner = Instantiate(bossSpawner);
         CheckIfLoadGame();
         getTotalExpToLevelUp();
+        AddExp(0);
     }
     bool loadDataFromLastGame;
     [SerializeField]
@@ -65,10 +78,9 @@ public class SceneManager : MonoBehaviour
         //Debug.Log(PlayerLevel);
         Debug.Log("TO-DO: Them ham tinh kinh nghiem va cho nguoi choi len cap");
         Debug.Log("Hien tai gia lap nguoi choi len level moi 2s");
-        if (Time.time >= SimulateTime && PlayerLevel < 10)
+        if (CurrentExp >= TotalExpToNextLevel)
         {
-            PlayerLevel += 1;
-            SimulateTime = Time.time + 2f;
+            CurrentExp = CurrentExp - TotalExpToNextLevel;
             PlayerLevelUp();
         }
         //spawn boss moi khi nhan vat tang 5 level
@@ -95,7 +107,7 @@ public class SceneManager : MonoBehaviour
     /// check xem nguoi choi chon load game hay choi moi o main menu sau do thuc hien hanh dong tuong ung
     /// recommend man 1 luu xuong PlayerPrefs 1 bien ten isLoadGame
     /// </summary>
-    private void CheckIfLoadGame()
+    /*private void CheckIfLoadGame()
     {
         SaveGameManager saveGameManager = GetComponent<SaveGameManager>();
         CharacterStatus characterStatus = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterStatus>();
@@ -117,7 +129,7 @@ public class SceneManager : MonoBehaviour
 
             }
         //}
-    }
+    }*/
     private void SaveData()
     {
         CharacterManager character_Skill = GetComponent<CharacterManager>();
@@ -145,6 +157,10 @@ public class SceneManager : MonoBehaviour
     {
         Debug.Log("TO-DO: Them kha nang tang exp tu drop item");
         CurrentExp += value;
+        foreach (IPlayerObserver observer in observers)
+        {
+            observer.OnPlayerExperienceGained(CurrentExp);
+        }
     }
 
     internal int getTotalExpToLevelUp()
@@ -155,21 +171,35 @@ public class SceneManager : MonoBehaviour
         {
             solveForRequiredExp += (int)Mathf.Floor(levelCylce + 300 * Mathf.Pow(2, levelCylce / 7));
         }
-        Debug.Log(solveForRequiredExp);
         TotalExpToNextLevel = solveForRequiredExp / 4;
+        foreach (IPlayerObserver observer in observers)
+        {
+            observer.OnPlayerTotalExperienceChanged(TotalExpToNextLevel);
+        }
         return TotalExpToNextLevel;
     }
     public void increaseExpForEnemy(EnemyStatus enemyStatus)
     {
-        Debug.Log("Tp-do: tinh exp cho quai tuy loai");
+        int numberOfEnemy = 0;
+        int expForEachEnemy;
+        Debug.Log("To-do: tinh exp cho quai tuy loai");
         bool isBoss = enemyStatus.GetType() == typeof(BossStatus);
-        if (enemyStatus.BaseStats.EnemyType == 1)
+        if (isBoss)
         {
-
+            expForEachEnemy = TotalExpToNextLevel;
         }
-        else if(enemyStatus.BaseStats.EnemyType == 2)
+        else
         {
-
+            if (enemyStatus.BaseStats.EnemyType == 1)
+            {
+                numberOfEnemy = PlayerLevel + 30 * PlayerLevel / (PlayerLevel + 1);
+            }
+            else if(enemyStatus.BaseStats.EnemyType == 2)
+            {
+                numberOfEnemy = PlayerLevel + 32 * PlayerLevel / (PlayerLevel + 1);
+            }
+            expForEachEnemy = TotalExpToNextLevel / numberOfEnemy;
         }
+        AddExp(expForEachEnemy);
     }
 }
